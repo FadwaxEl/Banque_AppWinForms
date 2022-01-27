9fac2aa96897bcdaed922b2484978320fa4fa721
unknown <fadwaelalaoui0@gmail.com>
2022-01-27 22:39:18 +0100

    thirdcommit

--- banqueInterface/Form1.Designer.cs
+++ banqueInterface/Form1.Designer.cs
@@ -1,16 +1,16 @@
-﻿namespace banqueInterface
+﻿
+namespace banqueInterface
 {
     partial class Form1
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+    { /// <summary>
+      /// Variable nécessaire au concepteur.
+      /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Nettoyage des ressources utilisées.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +20,123 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Code généré par le Concepteur Windows Form
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.Label();
+            this.login = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(467, 162);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(150, 30);
+            this.textBox3.TabIndex = 15;
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(352, 165);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(109, 25);
+            this.password.TabIndex = 14;
+            this.password.Text = "Password :";
+            // 
+            // login
+            // 
+            this.login.AutoSize = true;
+            this.login.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login.Location = new System.Drawing.Point(352, 100);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(71, 25);
+            this.login.TabIndex = 13;
+            this.login.Text = "Login :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(467, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 30);
+            this.textBox1.TabIndex = 12;
+            // 
+            // login_btn
+            // 
+            this.login_btn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.login_btn.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.login_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.login_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_btn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.login_btn.Location = new System.Drawing.Point(563, 214);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(54, 50);
+            this.login_btn.TabIndex = 11;
+            this.login_btn.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(292, 449);
+            this.panel1.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(709, 449);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.login_btn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.Label login;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
+
 
