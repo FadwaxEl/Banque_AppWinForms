8243e43ff0db3a4097ca2f526db45b4900777e9e
unknown <salaheddineabid99@gmail.com>
2022-01-27 23:14:42 +0100

    second commit

---
+++ appBanque/CompteEpargne.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appBanque
+{
+    public class CompteEpargne:Compte
+    {
+        private double tauxInteret;
+        public CompteEpargne(int id,Client client,Devise devise,double tauxInteret):base(id,client,devise)
+        {
+            this.tauxInteret = tauxInteret;
+        }
+    }
+}
